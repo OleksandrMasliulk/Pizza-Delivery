c6aeda08e55ce2d8f15314a5c171a8ac7c175318
odrmasliuk <odr.masliuk@gmail.com>
2022-10-29 16:00:34 +0200

    Lecture 2 | Done

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 0, .1f);
+        transform.Translate(0, .01f, 0);
     }
 }
