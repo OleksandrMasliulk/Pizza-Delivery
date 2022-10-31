456a74292a7dd775917952151ffa9b31f7b6fac9
odrmasliuk <odr.masliuk@gmail.com>
2022-10-31 17:57:03 +0100

    Lecture 16 | Done

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Delivery : MonoBehaviour
 {
     private bool _hasPizza = false;
     [SerializeField] private float _destroyPizzaDelay;
+
+    [SerializeField] private Color32 _defaultColor;
+    [SerializeField] private Color32 _hasPizzaColor;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake() 
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() 
+    {
+        _spriteRenderer.color = _defaultColor;
+    }
 
     private void OnCollisionEnter2D(Collision2D other) 
     {
@@ -16,12 +32,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Pizza picked up!");
             _hasPizza = true;
+            _spriteRenderer.color = _hasPizzaColor;
             Destroy(other.gameObject, _destroyPizzaDelay);
         }
 
         if (other.CompareTag("Customer") && _hasPizza)
         {
             Debug.Log("Pizza delivered!");
+            _spriteRenderer.color = _defaultColor;
             _hasPizza = false;
         }
     }
